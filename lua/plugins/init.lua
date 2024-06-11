c29e2f34dd447c0dcaf9b0c7bc4b50aebfa98b29
Arseniy Muravyev <arseniymuravyev@MacBook-Air-Arseniy.local>
2024-06-11 15:05:32 +0300

    nvchad

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -122,6 +122,11 @@ return {
         view = {
           width = 23,
           side = "left",
+          mappings = {
+            list = {
+              { key = "A", action = "create" },
+            },
+          },
         },
       }
     end,
@@ -145,12 +150,9 @@ return {
             require("luasnip").lsp_expand(args.body)
           end,
         },
-        mapping = cmp.mapping.preset.insert {
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<Tab>"] = cmp.mapping.confirm { select = true },
+        mapping = {
+          ["<CR>"] = cmp.mapping.confirm { select = true },
+          ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
         },
         sources = cmp.config.sources {
           { name = "nvim_lsp" },
