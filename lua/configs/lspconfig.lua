0c077d78f45cb31d7ff6012e95e04c7c8538ef7b
ArseniyMuravyev <aamuravyev@icloud.com>
2024-06-13 07:58:20 +0300

    nvchad

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "emmet_ls" }
+local servers = { "html", "cssls", "emmet_ls", "typescript-language-server" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,5 +1,4 @@
 -- Установите mapleader
-vim.g.mapleader = " "
 
 -- Импортируйте привязки клавиш NVChad
 require "nvchad.mappings"
@@ -10,6 +9,4 @@ local map = vim.keymap.set
 -- Привязки клавиш
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
-map("i", "jk", "<ESC>")
 map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit Vim" })
-map("n", "A", ":NvimTreeCreate<CR>", { desc = "Create file or directory in file tree" })
